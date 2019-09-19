09e94569cc693861ae2634f7b68a9986d26f8068
c6burns <chris@fursman.com>
2019-09-19 12:51:26 -0700

    removed private decl

--- include/tn/dso.h
+++ include/tn/dso.h
@@ -13,7 +13,6 @@ typedef struct tn_dso_s {
 } tn_dso_t;
 
 tn_dso_state_t tn_dso_state(tn_dso_t *dso);
-void tn_dso_state_set(tn_dso_t *dso, tn_dso_state_t state);
 int tn_dso_setup(tn_dso_t *dso, const char *so_name);
 int tn_dso_symbol(tn_dso_t *dso, const char *sym_name, void **out_fn_ptr);
 int tn_dso_cleanup(tn_dso_t *dso);
